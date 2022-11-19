94a75c51e303b79cbcd96e95f09c5515668a5872
Siddharth Sircar <111794453+spongeMouse93@users.noreply.github.com>
2022-11-19 11:43:45 -0500

    Create AddProduct.java

---
+++ src/warehouse/AddProduct.java
@@ -0,0 +1,39 @@
+package warehouse;
+
+import java.util.*;
+import java.io.*;
+
+//Used to test the warehouse add product method
+public class AddProduct{
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    PrintWriter out = new PrintWriter(System.out);
+    Warehouse w = new Warehouse();
+    try{
+      sc = new Scanner(new File(args[0]), "UTF-8");
+      sc.useLocale(Locale.US);
+    }catch (IOException e){
+      e.printStackTrace();
+    }
+    try{
+      out = new PrintWriter(new FileOutputStream(new File(args[1])), true);
+    }catch (IOException e){
+      try{
+        File f = new File(args[1]);
+        f.createNewFile();
+        out = new PrintWriter(new FileOutputStream(f), true);
+      }catch (IOException e2){
+        e2.printStackTrace();
+      }
+    }
+    int numCommands = sc.nextInt();
+    for (int i = 1; i <= numCommands; i++){
+      int day = sc.nextInt(), id = sc.nextInt();
+      String name = sc.next();
+      int stock = sc.nextInt(), demand = sc.nextInt();
+      w.addProduct(id, name, stock, day, demand);
+    }
+    out.println(w);
+    out.flush();
+  }
+}
