a1da303957e85a5c08d24f2512a1de61e44e033e
Siddharth Sircar <111794453+spongeMouse93@users.noreply.github.com>
2022-11-19 12:59:45 -0500

    Create DeleteProduct.java

---
+++ src/warehouse/DeleteProduct.java
@@ -0,0 +1,44 @@
+package warehouse;
+
+import java.util.*;
+import java.io.*;
+
+public class DeleteProduct{
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    PrintWriter out = new PrintWriter(System.out);
+    Warehouse w = new Warehouse();
+    try{
+      sc = new Scanner(new File(args[0]));
+      sc.useLocale(Locale.US);
+    }catch (IOException e){
+      e.printStackTrace();
+    }
+    try{
+      out = new PrintWriter(new FileOutputStream(new File(args[1])), true);
+    }catch (IOException e1){
+      try{
+        File f = new File(args[1]);
+        f.createNewFile();
+        out = new PrintWriter(new FileOutputStream(f), true);
+      }catch (IOException e2){
+        e2.printStackTrace();
+      }
+    }
+    int numCommands = sc.nextInt();
+    for (int i = 1; i <= numCommands; i++){
+      String command = sc.next();
+      if (command.equals("add")){
+        int day = sc.nextInt(), id = sc.nextInt();
+        String name = sc.next();
+        int stock = sc.nextInt(), demand = sc.nextInt();
+        w.addProduct(id, name, stock, day, demand);
+      }else{
+        int id = sc.nextInt();
+        w.deleteProduct(id);
+      }
+    }
+    out.println(w);
+    out.flush();
+ }
+}
