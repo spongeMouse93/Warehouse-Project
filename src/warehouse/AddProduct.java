1a476bf3b9973d39a1268e4d8a5f8c526156cb01
Siddharth Sircar <111794453+spongeMouse93@users.noreply.github.com>
2022-12-06 09:32:09 -0500

    Update AddProduct.java

--- src/warehouse/AddProduct.java
+++ src/warehouse/AddProduct.java
@@ -6,6 +6,10 @@ import java.io.*;
 //Used to test the warehouse add product method
 public class AddProduct{
   public static void main(String[] args){
+    if (args.length < 2){
+      System.err.println("Execute: java AddProduct <INput file> <OUTput file>");
+      return;
+    }
     Scanner sc = new Scanner(System.in);
     PrintWriter out = new PrintWriter(System.out);
     Warehouse w = new Warehouse();
