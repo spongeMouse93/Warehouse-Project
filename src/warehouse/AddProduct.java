9690acc1ef36d6ae28db496f2ab9928b36529f7a
Siddharth Sircar <111794453+spongeMouse93@users.noreply.github.com>
2024-05-09 12:21:51 -0400

    Update AddProduct.java

--- src/warehouse/AddProduct.java
+++ src/warehouse/AddProduct.java
@@ -17,7 +17,8 @@ public class AddProduct{
       sc = new Scanner(new File(args[0]), "UTF-8");
       sc.useLocale(Locale.US);
     }catch (IOException e){
-      e.printStackTrace();
+      System.err.println("Could not find input file.");
+      return;
     }
     try{
       out = new PrintWriter(new FileOutputStream(new File(args[1])), true);
