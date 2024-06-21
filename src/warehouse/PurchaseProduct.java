89346999127cac5076cc22c664e334ce205d0f1b
Siddharth Sircar <111794453+spongeMouse93@users.noreply.github.com>
2024-06-21 15:15:49 -0400

    Update PurchaseProduct.java

--- src/warehouse/PurchaseProduct.java
+++ src/warehouse/PurchaseProduct.java
@@ -5,7 +5,7 @@ import java.io.*;
 
 public class PurchaseProduct {
     public static void main(String[] args) {
-        if (args.length < 2){
+        if (args.length != 2){
             System.err.println("Execute: java PurchaseProduct <INput file> <OUTput file>");
             return;
         }
