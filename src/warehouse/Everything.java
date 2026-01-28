49313c8e868a84dee78d6fe4026da75fb0d9befd
Siddharth Sircar <111794453+spongeMouse93@users.noreply.github.com>
2026-01-27 20:43:07 -0500

    Update Everything.java

--- src/warehouse/Everything.java
+++ src/warehouse/Everything.java
@@ -2,6 +2,7 @@ package warehouse;
 
 import java.util.*;
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 
 public class Everything{
   public static void main(String[] args){
@@ -13,7 +14,7 @@ public class Everything{
     PrintWriter out = new PrintWriter(System.out);
     Warehouse w = new Warehouse();
     try{
-      sc = new Scanner(new File(args[0]));
+      sc = new Scanner(new File(args[0]), StandardCharsets.UTF_8);
       sc.useLocale(Locale.US);
     }catch (IOException e){
       System.err.println("Could not find input file.");
