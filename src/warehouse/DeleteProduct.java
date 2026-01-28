d665aa0c69dd82669cff1831489f51964dca196b
Siddharth Sircar <111794453+spongeMouse93@users.noreply.github.com>
2026-01-27 20:39:56 -0500

    Update DeleteProduct.java

--- src/warehouse/DeleteProduct.java
+++ src/warehouse/DeleteProduct.java
@@ -2,6 +2,7 @@ package warehouse;
 
 import java.util.*;
 import java.io.*;
+import java.ni.charset.StandardCharsets;
 
 public class DeleteProduct{
   public static void main(String[] args){
@@ -13,7 +14,7 @@ public class DeleteProduct{
     PrintWriter out = new PrintWriter(System.out);
     Warehouse w = new Warehouse();
     try{
-      sc = new Scanner(new File(args[0]));
+      sc = new Scanner(new File(args[0]), StandardCharsets.UTF_8);
       sc.useLocale(Locale.US);
     }catch (IOException e){
       System.err.println("Could not find input file.");
